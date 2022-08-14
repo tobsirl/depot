7eca904b0b8b7ce19e89d767c7df7dd44a741abe
Paul Tobin <tobster111@gmail.com>
2022-08-14 11:38:41 +0100

    Random title update

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -41,7 +41,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test 'should update product' do
     patch product_url(@product),
           params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price,
-                               title: @product.title } }
+                               title: @title } }
     assert_redirected_to product_url(@product)
   end
 
