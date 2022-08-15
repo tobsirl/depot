6bffddabd03a91c1c9d5d93909fcc92e37cc6a91
Paul Tobin <tobster111@gmail.com>
2022-08-15 11:04:18 +0100

    Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_14_082333) do
+ActiveRecord::Schema.define(version: 2022_08_15_100048) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
