672582b92ebe8172fa79bb40263d54b2cf075093
Paul Tobin <tobster111@gmail.com>
2022-08-15 11:53:53 +0100

    Updating tests 2

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,7 +17,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { product_id: { product_id: products(:ruby).id } }
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
 
     follow_redirect!
