c9a73c625f7e09378714e41aca4bc1295fa7db3b
Paul Tobin <tobster111@gmail.com>
2022-08-15 11:36:41 +0100

    Add button styling

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,6 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]
   before_action :set_line_item, only: %i[ show edit update destroy ]
 
   # GET /line_items or /line_items.json
