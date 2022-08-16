bb40e1f15d0359a3f268c57c8cf5b2401718de83
Paul Tobin <tobster111@gmail.com>
2022-08-16 08:55:35 +0100

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_074127) do
+ActiveRecord::Schema.define(version: 2022_08_16_074905) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
