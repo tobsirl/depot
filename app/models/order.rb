b6f0f6fbeeeb693cd299f96e5a9d1a3d19175110
Paul Tobin <tobster111@gmail.com>
2022-08-18 11:53:53 +0100

    Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
+  has_many :line_items, dependent: :destroy
 
   enum pay_type: {
     "Check" => 0,
