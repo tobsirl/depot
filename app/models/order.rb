5e118d0cc3f4c64f580628cccacf3b3b2a85d56d
Paul Tobin <tobster111@gmail.com>
2022-08-18 11:11:06 +0100

    Validate order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+
   enum pay_type: {
     "Check" => 0,
     "Credit card" => 1,
