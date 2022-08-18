2be36186e9ce5418cc504e1c63275bc3ae3e831c
Paul Tobin <tobster111@gmail.com>
2022-08-18 11:53:07 +0100

    LineItem model

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 class LineItem < ApplicationRecord
+  belongs_to :order, optional: true
   belongs_to :product
-  belongs_to :cart
+  belongs_to :cart, optional: true
 
   def total_price
     product.price * quantity
