f7dfc1dac292d977ace582271cceb2d7402e7175
Paul Tobin <tobster111@gmail.com>
2022-08-23 14:17:52 +0100

    ChargeOrderJob

--- app/jobs/charge_order_job.rb
+++ app/jobs/charge_order_job.rb
@@ -1,7 +1,9 @@
 class ChargeOrderJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(order,pay_type_params)
+
+    order.charge!(pay_type_params)
+
   end
 end
