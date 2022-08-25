e322f732d9e4e2bd36ab6ae97c431f14525210c9
Paul Tobin <tobster111@gmail.com>
2022-08-25 08:51:48 +0100

    callback before_action authorize application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
+    # ...
+
+  protected
+
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
 end
